b0f6dcb755423db0c62ac21ad2dc52c0f0541a13
Prathiba-hub <sd1768135@gmail.com>
2026-01-05 13:19:43 +0530

    Update Loops.py

--- Day 1/Loops.py
+++ Day 1/Loops.py
@@ -1,4 +1,10 @@
-n = int(input());
+# Day 1
+# Topic: Loop
+# Problem: Print square of numbers
+# Platform: HackerRank
+# Language: Python
+
+n = int(input())
 
 for i in range(n):
-    print(i*i);
+    print(i * i)
