d2f5907c05aa163482b11f10d7c606aa7b11483b
Prathiba-hub <sd1768135@gmail.com>
2026-01-05 13:31:51 +0530

    Update Splitting into the teams.py

--- Day 3/Splitting into the teams.py
+++ Day 3/Splitting into the teams.py
@@ -1,5 +1,16 @@
-n=int(input())
-n1=int(input())
-ans=n%n1
-ans2=n//n1
-print(f"The number of friends in each team is {ans2} and left out is {ans}")
+# Day 3
+# Problem: Splitting into the Teams
+# Platform: HackerRank
+# Topic: Python Basics / Arithmetic
+# Language: Python
+
+# Input total number of students and team size
+n = int(input())   # Total students
+n1 = int(input())  # Team size
+
+# Calculate number of students in each team and leftover
+left_out = n % n1
+per_team = n // n1
+
+# Print the result
+print(f"The number of friends in each team is {per_team} and left out is {left_out}")
