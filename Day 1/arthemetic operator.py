90e216d9a750166a1671b10bc695ef30a9d935f9
Prathiba-hub <sd1768135@gmail.com>
2026-01-05 13:22:59 +0530

    Update arithmetic operator.py

--- Day 1/arthemetic operator.py
+++ Day 1/arthemetic operator.py
@@ -1,6 +1,12 @@
-num1 = int(input());
-num2 = int(input());
+# Day 1
+# Problem: Arithmetic Operators
+# Platform: HackerRank
+# Topic: Python Basics
+# Language: Python
 
-print(num1 + num2);
-print(num1 - num2);
-print(num1 * num2);
+num1 = int(input())
+num2 = int(input())
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
