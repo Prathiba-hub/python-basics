7f6d550109a92e8b6b3b3517806dfd4599d39191
Prathiba-hub <sd1768135@gmail.com>
2026-01-05 13:27:15 +0530

    Update Student Details.py

--- Day 2/Student Details.py
+++ Day 2/Student Details.py
@@ -1,11 +1,18 @@
-name = input();
-age = int(input());
-cgpa = float(input());
-grade = input();
+# Day 2
+# Problem: Student Details
+# Platform: HackerRank
+# Topic: Python Basics
+# Language: Python
 
+name = input()
+age = int(input())
+cgpa = float(input())
+grade = input()
+
+# Truncate CGPA to 2 decimal places
 cgpa_truncated = int(cgpa * 100) / 100
 
-print("Name:",name);
-print("Age:",age);
-print("CGPA:",cgpa_truncated);
-print("Grade:",grade);
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa_truncated)
+print("Grade:", grade)
