d5c467949607f3c204dcf64d2deba96ab1ecb756
Prathiba-hub <sd1768135@gmail.com>
2026-01-05 13:25:52 +0530

    Update Fencing the Ground.py

--- Day 2/Fencing the Ground.py
+++ Day 2/Fencing the Ground.py
@@ -1,6 +1,14 @@
-l=int(input())
-b=int(input())
-peri=2*(l+b)
-area=l*b
-print(f"The required length is {peri} m")
+# Day 2
+# Problem: Fencing the Ground
+# Platform: HackerRank
+# Topic: Python Basics
+# Language: Python
+
+l = int(input())
+b = int(input())
+
+perimeter = 2 * (l + b)
+area = l * b
+
+print(f"The required length is {perimeter} m")
 print(f"The required area of carpet is {area} sqm")
