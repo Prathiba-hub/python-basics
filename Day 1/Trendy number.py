a9a06eff70595846e0a9cdcbfb24b0c43d83b680
Prathiba-hub <sd1768135@gmail.com>
2026-01-05 13:21:33 +0530

    Update Trendy number.py

--- Day 1/Trendy number.py
+++ Day 1/Trendy number.py
@@ -1,7 +1,14 @@
-val=int(input())
-ans=(val//10)%10
-if 100<=val and val<=999:
-    if ans%3==0:
+# Day 1
+# Problem: Trendy Number
+# Platform: HackerRank
+# Topic: Conditional Statements
+# Language: Python
+
+val = int(input())
+
+if 100 <= val <= 999:
+    middle_digit = (val // 10) % 10
+    if middle_digit % 3 == 0:
         print("Trendy Number")
     else:
         print("Not a Trendy Number")
