3174cbcb5b50f6f10ace13b167a5670d126974d9
Prathiba-hub <sd1768135@gmail.com>
2026-01-05 13:26:23 +0530

    Update Round Off.py

--- Day 2/Round Off.py
+++ Day 2/Round Off.py
@@ -1,5 +1,13 @@
-import math 
-val=float(input())
-print(math.floor(val))
-print(math.ceil(val))
-print(math.trunc(val))
+# Day 2
+# Problem: Round Off
+# Platform: HackerRank
+# Topic: Python Math Functions
+# Language: Python
+
+import math
+
+val = float(input())
+
+print(math.floor(val))   # Floor value
+print(math.ceil(val))    # Ceil value
+print(math.trunc(val))   # Truncated value
