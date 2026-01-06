2c2c223f8c8c08a41bf8b089ea3802086adf2253
Prathiba-hub <sd1768135@gmail.com>
2026-01-06 10:17:11 +0530

    Day 7: Python print prime numbers in a range

--- Day 7/Print Prime Numbers in a range.py
+++ Day 7/Print Prime Numbers in a range.py
@@ -1,15 +1,24 @@
-n=int(input())
-if 1<n<109:
-    for n in range(2,n+1):
-        flag=True
-        if n<2:
-            continue
-        if n==2:
-            print("2",end=" ")
-            continue
-        for i in range(2,n):
-            if n%i==0:
-                flag=False
+# Day 7
+# Problem: Print Prime Numbers in a Range
+# Platform: HackerRank
+# Topic: Python Loops / Conditional Statements
+# Language: Python
+
+# Input the upper limit
+n = int(input("Enter the upper limit: "))
+
+# Check valid range
+if 1 < n < 109:
+    # Loop through numbers from 2 to n
+    for num in range(2, n + 1):
+        is_prime = True
+
+        # Check if num is divisible by any number from 2 to sqrt(num)
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
                 break
-        if flag:
-            print(n,end=" ")
+
+        # Print prime numbers
+        if is_prime:
+            print(num, end=" ")
