c55a1c401f2ed9d3d625ae0c9303bd29b3622bab
Prathiba-hub <sd1768135@gmail.com>
2026-01-06 10:27:42 +0530

    Day 9: Python Kaprekar number check

--- Day 9/Kaprekar number.py
+++ Day 9/Kaprekar number.py
@@ -1,14 +1,31 @@
-n=int(input())
-s=n**2
-num=n
-sum=0
-while n>0:
-    n=n//10
-    sum+=1
-right=s%(10**sum)
-left=s//(10**sum)
-k=right+left   
-if k==num:
+# Day 9
+# Problem: Kaprekar Number
+# Platform: HackerRank
+# Topic: Python Loops / Math
+# Language: Python
+
+# Input a number
+num = int(input("Enter a number: "))
+
+# Calculate square of the number
+square = num ** 2
+
+# Count number of digits in the original number
+temp = num
+digits = 0
+while temp > 0:
+    temp //= 10
+    digits += 1
+
+# Split square into right and left parts
+right = square % (10 ** digits)
+left = square // (10 ** digits)
+
+# Sum left and right parts
+k = right + left
+
+# Check if Kaprekar Number
+if k == num:
     print("Kaprekar Number")
 else:
     print("Not a Kaprekar Number")
