2e62e4c025d8bd341a55a6eb38ab73125148e5c5
Prathiba-hub <sd1768135@gmail.com>
2026-01-06 10:41:55 +0530

    Day 10: Python sort array in ascending order

--- Day 10/Ascending order.py
+++ Day 10/Ascending order.py
@@ -1,8 +1,21 @@
-arr=[]
-num=int(input())
-for i in range(num):
+# Day 10
+# Problem: Ascending Order
+# Platform: HackerRank
+# Topic: Python Lists / Sorting
+# Language: Python
+
+# Input size of the array
+num = int(input("Enter the number of elements: "))
+
+# Input array elements
+arr = []
+for _ in range(num):
     arr.append(int(input()))
-arr=sorted(arr)
+
+# Sort the array in ascending order
+arr.sort()
+
+# Print the sorted array
 print("The Sorted array is:")
-for i in arr:
-    print(i)
+for elem in arr:
+    print(elem)
