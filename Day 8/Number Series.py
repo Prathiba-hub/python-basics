a61b423462990b1ba5f86e31e80cf0b6fcbed6ef
Prathiba-hub <sd1768135@gmail.com>
2026-01-06 10:23:45 +0530

    Day 8: Python number series problem

--- Day 8/Number Series.py
+++ Day 8/Number Series.py
@@ -1,7 +1,16 @@
-num=int(input())
-for i in range(1,num+1):
-    if i%2==0:
-        ans=(i**2)-2
+# Day 8
+# Problem: Number Series
+# Platform: HackerRank
+# Topic: Python Loops / Arithmetic
+# Language: Python
+
+# Input the number of terms
+num = int(input("Enter the number of terms: "))
+
+# Generate the series
+for i in range(1, num + 1):
+    if i % 2 == 0:
+        ans = (i ** 2) - 2  # Even term formula
     else:
-        ans=(i**2)-1
-    print(ans,end=" ")
+        ans = (i ** 2) - 1  # Odd term formula
+    print(ans, end=" ")
