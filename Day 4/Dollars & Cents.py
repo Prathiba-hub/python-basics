1d5af4a51adca2d3bd8597a8e7a43291d571836c
Prathiba-hub <sd1768135@gmail.com>
2026-01-06 09:55:55 +0530

    Update Dollars & Cents.py

--- Day 4/Dollars & Cents.py
+++ Day 4/Dollars & Cents.py
@@ -1,12 +1,24 @@
-d1=int(input())
-c1=int(input())
-d2=int(input())
-c2=int(input())
-val1=d1+d2
-val2=c1+c2
-if(val2>=100):
-    val2-=100
-    val1+=1
-print(val1)
-print(val2)
-    
+# Day 4
+# Problem: Dollars & Cents
+# Platform: HackerRank
+# Topic: Python Basics / Arithmetic
+# Language: Python
+
+# Input dollars and cents
+d1 = int(input())
+c1 = int(input())
+d2 = int(input())
+c2 = int(input())
+
+# Add dollars and cents
+total_dollars = d1 + d2
+total_cents = c1 + c2
+
+# Convert excess cents to dollars
+if total_cents >= 100:
+    total_cents -= 100
+    total_dollars += 1
+
+# Output result
+print(total_dollars)
+print(total_cents)
