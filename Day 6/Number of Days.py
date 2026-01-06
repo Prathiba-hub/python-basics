37d070bf0e2fddae985d0b403e9bd172f4d9ebf0
Prathiba-hub <sd1768135@gmail.com>
2026-01-06 10:09:08 +0530

    Update Number of Days.py

--- Day 6/Number of Days.py
+++ Day 6/Number of Days.py
@@ -1,16 +1,27 @@
-year=int(input())
-month=int(input())
-val=[1,3,5,7,8,10,12]
-val2=[4,6,9,11]
-if 1900<=year<=9999 and 1<=month<=12:
-    if month in val:
+# Day 6
+# Problem: Number of Days
+# Platform: HackerRank
+# Topic: Python Conditional Statements / Leap Year
+# Language: Python
+
+# Input year and month
+year = int(input())
+month = int(input())
+
+# Lists of months with 31 and 30 days
+months_31 = [1, 3, 5, 7, 8, 10, 12]
+months_30 = [4, 6, 9, 11]
+
+# Check for valid year and month
+if 1900 <= year <= 9999 and 1 <= month <= 12:
+    if month in months_31:
         print("31 Days")
-    if month==2:
-        if year%4==0 and year%100!=0 or year%400==0:
+    elif month in months_30:
+        print("30 Days")
+    else:  # February
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
             print("29 Days")
         else:
             print("28 Days")
-    if month in val2:
-        print("30 Days")
 else:
-    print("0")
+    print("0")  # Invalid input
