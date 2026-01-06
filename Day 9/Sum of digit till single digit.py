464a062ecc76cf3cbfbdfa4ce950e19718f9c72b
Prathiba-hub <sd1768135@gmail.com>
2026-01-06 10:30:15 +0530

    Update Sum of digit till single digit.py

--- Day 9/Sum of digit till single digit.py
+++ Day 9/Sum of digit till single digit.py
@@ -1,13 +1,19 @@
-num=int(input())
-num_str=str(num)
-m=0
-for i in num_str:
-    digit=int(i)
-    m=m+digit
-    n=m//10#44//10=4
-    first=m%10
-    A=n+first
-print(A)
- 
+# Day 9
+# Problem: Sum of Digits Till Single Digit
+# Platform: HackerRank
+# Topic: Python Loops / Math
+# Language: Python
 
-    
+# Input a number
+num = int(input("Enter a number: "))
+
+# Compute sum of digits repeatedly until a single digit
+temp = num
+while temp >= 10:
+    sum_digits = 0
+    for digit in str(temp):
+        sum_digits += int(digit)
+    temp = sum_digits
+
+# Print the single-digit result
+print(temp)
