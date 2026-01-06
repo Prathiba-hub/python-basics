e538a07af21995373a056c4d73671f50f84978ea
Prathiba-hub <sd1768135@gmail.com>
2026-01-06 10:26:42 +0530

    Day 9: Python inverted right-angled triangle pattern

--- Day 9/Inverted right-angled triangle.py
+++ Day 9/Inverted right-angled triangle.py
@@ -1,6 +1,16 @@
-num=int(input())
-while num>=1:
-    for i in range(0,num):
-        print("*",end="")
-    print()
-    num-=1
+# Day 9
+# Problem: Inverted Right-Angled Triangle
+# Platform: HackerRank
+# Topic: Python Loops / Patterns
+# Language: Python
+
+# Input the number of rows
+num = int(input("Enter the number of rows: "))
+
+# Loop from num down to 1
+while num >= 1:
+    # Print stars in each row
+    for i in range(num):
+        print("*", end="")
+    print()  # Move to next line
+    num -= 1
