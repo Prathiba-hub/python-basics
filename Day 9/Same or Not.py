4c15be31816e807c210e7620b780e8cd5ce3165b
Prathiba-hub <sd1768135@gmail.com>
2026-01-06 10:29:16 +0530

    Day 9: Python compare two lists for sum and length

--- Day 9/Same or Not.py
+++ Day 9/Same or Not.py
@@ -1,18 +1,23 @@
-arr1=[]
-val1=int(input())
-val2=int(input())
-for i in range(val1):
-    ans=int(input())
-    arr1.append(ans)
-arr2=[]
-for i in range(val2):
-    ans1=int(input())
-    arr2.append(ans1)
-add1=sum(arr1)
-add2=sum(arr2)
-length1=len(arr1)
-length2=len(arr2)
-if add1==add2 and length1==length2:
+# Day 9
+# Problem: Same or Not
+# Platform: HackerRank
+# Topic: Python Lists / Conditional Statements
+# Language: Python
+
+# Input size and elements of first list
+arr1 = []
+val1 = int(input("Enter number of elements in first list: "))
+for _ in range(val1):
+    arr1.append(int(input()))
+
+# Input size and elements of second list
+arr2 = []
+val2 = int(input("Enter number of elements in second list: "))
+for _ in range(val2):
+    arr2.append(int(input()))
+
+# Compare sum and length of the two lists
+if sum(arr1) == sum(arr2) and len(arr1) == len(arr2):
     print("Same")
 else:
     print("Not Same")
