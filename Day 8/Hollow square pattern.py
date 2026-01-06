00e69c22080a5f2da62f41a7f2e9156c42411353
Prathiba-hub <sd1768135@gmail.com>
2026-01-06 10:22:54 +0530

    Day 8: Python hollow square pattern

--- Day 8/Hollow square pattern.py
+++ Day 8/Hollow square pattern.py
@@ -1,8 +1,20 @@
-num=int(input())
-for x in range(num):
-    for y in range(num):
-        if x==0 or x==num-1 or y==0 or y==num-1:
-            print("*",end="")
+# Day 8
+# Problem: Hollow Square Pattern
+# Platform: HackerRank
+# Topic: Python Loops / Patterns
+# Language: Python
+
+# Input the size of the square
+num = int(input("Enter the size of the square: "))
+
+# Loop through each row
+for row in range(num):
+    # Loop through each column
+    for col in range(num):
+        # Print '*' for borders, space for inner area
+        if row == 0 or row == num - 1 or col == 0 or col == num - 1:
+            print("*", end="")
         else:
-            print(" ",end="")
+            print(" ", end="")
+    # Move to next line after each row
     print()
