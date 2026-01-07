89f8ed511a7447981798f47ee9cd5c6cd16e4feb
Prathiba-hub <sd1768135@gmail.com>
2026-01-07 11:37:42 +0530

    Update Online Game.py

--- Day_11/Online Game.py
+++ Day_11/Online Game.py
@@ -1,18 +1,31 @@
-n=int(input())
-a=list(map(int,input().split()))
-left=0
-right=n-1
-while left<right:
-    while a[left]%2==0:
-        left+=1
-    while a[right]%2!=0:
-        right-=1
-    if left<right:
-        t=a[left]
-        a[left]=a[right]
-        a[right]=t
-        left+=1
-        right-=1
+# Day 11
+# Problem: Online game
+# Platform: HackerRank
+# Topic: Python Arrays / Two Pointers
+# Language: Python
+
+# Input size of array
+n = int(input("Enter number of elements: "))
+
+# Input array elements
+a = list(map(int, input("Enter the elements: ").split()))
+
+left = 0
+right = n - 1
+
+# Two-pointer approach to segregate even and odd numbers
+while left < right:
+    while left < right and a[left] % 2 == 0:
+        left += 1
+    while left < right and a[right] % 2 != 0:
+        right -= 1
+
+    if left < right:
+        a[left], a[right] = a[right], a[left]
+        left += 1
+        right -= 1
+
+# Output
 print("Array after Segregation")
-for i in range(len(a)):
-    print(a[i],end=" ")
+for element in a:
+    print(element, end=" ")
