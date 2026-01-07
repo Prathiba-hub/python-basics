4314f78c5cc677b9c6aa0c4f4ba6fc8a6540346d
Prathiba-hub <sd1768135@gmail.com>
2026-01-07 11:38:33 +0530

    Day 11: Pair the container problem using two pointers

--- Day_11/Pair the Container.py
+++ Day_11/Pair the Container.py
@@ -1,16 +1,32 @@
-N=int(input())
-capacities=list(map(int,input().strip().split()))
-capacities.sort()
-pairs=[]
-left=0
-right=N-1
-while left<right:
-    pairs.append((capacities[right],capacities[left]))
-    left+=1
-    right-=1
-if left==right:
-    pairs.append((capacities[left],0))
-for pair in pairs:
-    print(pair[0],pair[1])
+# Day 11
+# Problem: Pair the Container
+# Platform: HackerRank
+# Topic: Python Arrays / Two Pointer Technique
+# Language: Python
 
-    
+# Input number of containers
+N = int(input("Enter number of containers: "))
+
+# Input container capacities
+capacities = list(map(int, input("Enter the capacities: ").split()))
+
+# Sort capacities
+capacities.sort()
+
+left = 0
+right = N - 1
+pairs = []
+
+# Pair largest with smallest
+while left < right:
+    pairs.append((capacities[right], capacities[left]))
+    left += 1
+    right -= 1
+
+# If one container remains unpaired
+if left == right:
+    pairs.append((capacities[left], 0))
+
+# Output result
+for pair in pairs:
+    print(pair[0], pair[1])
