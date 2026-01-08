35936dada2db9b1c0d9577d0a1bb70332ee3643f
Prathiba-hub <sd1768135@gmail.com>
2026-01-08 09:50:39 +0530

    Day 12: Matrix multiplication in Python

--- Day_12/Matrix Multiplication.py
+++ Day_12/Matrix Multiplication.py
@@ -1,16 +1,35 @@
-num1,num2=map(int,input().split())
-arr1=[]
-for i in range(num1):
-    l=list(map(int,input().split()))
-    arr1.append(l)
-arr2=[]
-for i in range(num2):
-    l=list(map(int,input().split()))
-    arr2.append(l)
-mul=[[0]*num2 for j in range(num2)]
-for i in range(num1):
-        for j in range(num2):
-            for k in range(num2):
-                mul[i][j] += arr1[i][k] * arr2[k][j]
-for m in mul:
-    print(' '.join(map(str,m)))
+# Day 12
+# Problem: Matrix Multiplication
+# Topic: Python Matrices
+# Language: Python
+
+# Input dimensions
+r1, c1 = map(int, input("Enter rows and columns of first matrix: ").split())
+r2, c2 = map(int, input("Enter rows and columns of second matrix: ").split())
+
+# Input first matrix
+mat1 = []
+for _ in range(r1):
+    mat1.append(list(map(int, input().split())))
+
+# Input second matrix
+mat2 = []
+for _ in range(r2):
+    mat2.append(list(map(int, input().split())))
+
+# Check if multiplication is possible
+if c1 != r2:
+    print("Matrix multiplication not possible")
+else:
+    # Initialize result matrix with zeros
+    result = [[0 for _ in range(c2)] for _ in range(r1)]
+
+    # Matrix multiplication logic
+    for i in range(r1):
+        for j in range(c2):
+            for k in range(c1):
+                result[i][j] += mat1[i][k] * mat2[k][j]
+
+    # Print result matrix
+    for row in result:
+        print(" ".join(map(str, row)))
