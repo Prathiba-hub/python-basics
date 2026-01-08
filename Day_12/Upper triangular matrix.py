3901d5ab0d5bc50253fb9e2716506be939b62d41
Prathiba-hub <sd1768135@gmail.com>
2026-01-08 09:58:03 +0530

    Day 12: Check upper triangular matrix

--- Day_12/Upper triangular matrix.py
+++ Day_12/Upper triangular matrix.py
@@ -1,16 +1,26 @@
-num=int(input())
-arr=[]
-for _ in range(num):
-    l=list(map(int,input().split()))
-    arr.append(l)
-utm=True
-for i in range(num):
+# Day 12
+# Problem: Upper Triangular Matrix
+# Topic: Python Matrices
+# Language: Python
+
+# Input size of square matrix
+n = int(input("Enter the size of the matrix: "))
+
+# Input matrix
+matrix = []
+for _ in range(n):
+    matrix.append(list(map(int, input().split())))
+
+# Check if matrix is upper triangular
+is_upper_triangular = True
+for i in range(n):
     for j in range(i):
-        if arr[i][j]!=0:
-            utm=False
-if utm==True:
+        if matrix[i][j] != 0:
+            is_upper_triangular = False
+            break
+
+# Output result
+if is_upper_triangular:
     print("Upper triangular matrix")
 else:
     print("Not an Upper triangular matrix")
-            
-        
