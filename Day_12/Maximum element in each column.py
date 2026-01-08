64f989701d80a76dc96a922ccc653d48c1fa32a0
Prathiba-hub <sd1768135@gmail.com>
2026-01-08 09:51:35 +0530

    Day 12: Find maximum element in each column of a matrix

--- Day_12/Maximum element in each column.py
+++ Day_12/Maximum element in each column.py
@@ -1,16 +1,21 @@
-n=int(input())
-m=int(input())
-arr=[]
-for i in range(n):
-    l=list(map(int,input().split()))
-    arr.append(l)
-for i in range(m):
-    maxi=arr[0][i]
-    for j in range(1,n):
-        if arr[j][i]>maxi:
-            maxi=arr[j][i]
-    print(maxi)
-        
-        
-    
-        
+# Day 12
+# Problem: Maximum Element in Each Column
+# Topic: Python Matrices
+# Language: Python
+
+# Input matrix dimensions
+n = int(input("Enter number of rows: "))
+m = int(input("Enter number of columns: "))
+
+# Input matrix
+matrix = []
+for _ in range(n):
+    matrix.append(list(map(int, input().split())))
+
+# Find and print maximum element in each column
+for col in range(m):
+    max_value = matrix[0][col]
+    for row in range(1, n):
+        if matrix[row][col] > max_value:
+            max_value = matrix[row][col]
+    print(max_value)
