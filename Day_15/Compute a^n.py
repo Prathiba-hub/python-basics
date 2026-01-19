214a7d41090983c0a984321003773230a0b5332e
Prathiba-hub <sd1768135@gmail.com>
2026-01-19 14:12:52 +0530

    Added Day 15 power of a number program

--- Day_15/Compute a^n.py
+++ Day_15/Compute a^n.py
@@ -1,5 +1,18 @@
+# Day 15
+# Problem: Compute a^n (Power of a Number)
+# Topic: Basic Mathematics
+# Language: Python
+
 import math
-num=int(input())
-num2=int(input())
-ans=math.pow(num,num2)
-print(f"The value of {num} power {num2} is {int(ans)}" )
+
+# Read base value
+num = int(input())
+
+# Read power value
+num2 = int(input())
+
+# Calculate power
+ans = math.pow(num, num2)
+
+# Display result
+print(f"The value of {num} power {num2} is {int(ans)}")
