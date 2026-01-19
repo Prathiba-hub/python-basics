405d9a206e4e0d958abe26315fd308f130d0ea15
Prathiba-hub <sd1768135@gmail.com>
2026-01-19 14:09:15 +0530

    Added Day 14 number of digits program

--- Day_14/Number of digits.py
+++ Day_14/Number of digits.py
@@ -1,8 +1,21 @@
+# Day 14
+# Problem: Number of Digits
+# Topic: Recursion
+# Language: Python
+
 def count_num(n):
-    if n<10:
+    # Base case: single digit
+    if n < 10:
         return 1
     else:
-        return 1 + count_num(n//10)
+        # Recursive call
+        return 1 + count_num(n // 10)
+
+# Read input from user
 num = int(input())
+
+# Count number of digits
 dig = count_num(num)
+
+# Display result
 print(f"The number of digits in {num} is {dig}")
