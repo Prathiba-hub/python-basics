6b69e2f8f6880a7238878131c0f9354c8734c2ae
Prathiba-hub <sd1768135@gmail.com>
2026-01-19 14:08:19 +0530

    Added Day 14 matrix rotation program

--- Day_14/Matrix Rotation.py
+++ Day_14/Matrix Rotation.py
@@ -1,8 +1,18 @@
-num=int(input())
-arr=[]
+# Day 14
+# Problem: Matrix Rotation (90 Degree Clockwise)
+# Topic: Python Matrices
+# Language: Python
+
+# Read size of the matrix
+num = int(input())
+
+# Read matrix elements
+arr = []
 for i in range(num):
-    arr.append(list(map(int,input().split())))
+    arr.append(list(map(int, input().split())))
+
+# Rotate the matrix and print the result
 for i in range(num):
-    for j in range(num-1,-1,-1):
-        print(arr[j][i],end=" ")
+    for j in range(num - 1, -1, -1):
+        print(arr[j][i], end=" ")
     print()
