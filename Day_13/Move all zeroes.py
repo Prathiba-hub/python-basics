f660e678912762a10434fce06cef4f2634fe531d
Prathiba-hub <sd1768135@gmail.com>
2026-01-19 14:03:33 +0530

    Update Move all zeroes.py

--- Day_13/Move all zeroes.py
+++ Day_13/Move all zeroes.py
@@ -1,9 +1,14 @@
+# Read number of strings
 n = int(input())
+
+# List to store strings
 strings = []
 
+# Read strings
 for _ in range(n):
     strings.append(input().strip())
 
+# Sort each string in reverse order and print
 for s in strings:
     sorted_string = ''.join(sorted(s, reverse=True))
     print(sorted_string)
